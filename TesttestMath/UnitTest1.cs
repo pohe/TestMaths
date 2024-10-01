87ab510d5f86e27fba893c6829f595c2570b235a
Poul Henriksen <POHE@zealand.dk>
2024-10-01 16:31:16 +0200

    Add project files.

---
+++ TesttestMath/UnitTest1.cs
@@ -0,0 +1,40 @@
+using TestMaths;
+
+namespace TesttestMath
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            //Arrange
+            BasicMaths bm = new BasicMaths();
+            //Act
+            double res = bm.Multiply(10, 10);
+            //Assert
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
